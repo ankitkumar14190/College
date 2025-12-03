c1c4d765b68cb7c52ec3d9c858ccfdfdaa2037b2
ankitkumar14190 <shyamraj2397@gmail.com>
2025-12-04 04:01:28 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,12 @@
+def recur_fact(n):
+ if n == 1:
+     return n
+ else:
+    return n*recur_fact(n-1)
+num=int(input("Enter a number:"))
+if num<0:
+    print("Sorry, Factorial does not exist for negative numbers")
+elif num==0:
+    print("The factorial of 0 is 1")
+else:
+    print("The factorial of", num, "is", recur_fact(num))
